8022290138390806e16f03b557edee19ee2f4b04
Wole Olapoju <35400817+woleolapoju@users.noreply.github.com>
2026-04-16 15:35:00 -0400

    Splash

--- SmartStock/obj/Debug/net10.0/SmartStock.AssemblyInfo.cs
+++ SmartStock/obj/Debug/net10.0/SmartStock.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartStock")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5973b6e7d7b92231e16f54657408a86e3084a35c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b6098704318fe03747c67aa5063ef3c5ff2bb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartStock")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartStock")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
